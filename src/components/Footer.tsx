8c37173e0a2618545c6a3970139963746206a3c1
Mauricio Antero <mjunior447@gmail.com>
2025-05-08 15:01:06 -0300

    feat: set dynamic current year; add padding for responsivity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -8,6 +6,8 @@ import {
   PiLinkedinLogoBold,
   PiTiktokLogoBold,
 } from "react-icons/pi";
+
+const currentYear = new Date().getFullYear();
 
 const socialMediaIcons = [
   { id: "tiktok", icon: <PiTiktokLogoBold size={26} />, href: "" },
@@ -26,7 +26,7 @@ const socialMediaIcons = [
 
 const Footer = () => {
   return (
-    <footer className="w-full h-40 border-t-2 border-preto1/30">
+    <footer className="w-full h-40 border-t-2 border-preto1/30 px-6">
       <div className="flex flex-col sm:flex-row gap-8 sm:gap-4 justify-between items-center max-w-[1440px] h-full m-auto p-4 md:p-0">
         <div className="flex items-center">
           <Image
@@ -37,9 +37,9 @@ const Footer = () => {
             className="border-2 border-preto1/75 rounded-3xl mr-3"
           />
           <p className="font-medium text-sm md:text-base">
-            &#169; 2025 - Todos os direitos reservados a Escrevendo na Quebrada.
-            Criado por Taina Alves, Thiago Alves, Gabryella Milan, Mauricio
-            Antero
+            &#169; {currentYear} - Todos os direitos reservados a Escrevendo na
+            Quebrada. Criado por Taina Alves, Thiago Alves, Gabryella Milan,
+            Mauricio Antero
           </p>
         </div>
 
