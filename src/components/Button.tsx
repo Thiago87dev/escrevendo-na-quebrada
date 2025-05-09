1dbe3ba716937d6f1e2dd9e5ea3735c6c6fcd450
Mauricio Antero <mjunior447@gmail.com>
2025-05-09 12:05:10 -0300

    feat: cria componente de botao

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps {
+  children: string;
+  className?: string;
+}
+
+const Button = ({ children, className }: ButtonProps) => {
+  return (
+    <button
+      className={`bg-roxo-escuro px-6 py-2 text-white rounded-4xl cursor-pointer hover:bg-roxo active:opacity-85 uppercase ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
