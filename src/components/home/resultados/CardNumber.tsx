686c1db222e8a9d065bfc91af355f63bb750e13c
Thiago Alves <96924815+Thiago87dev@users.noreply.github.com>
2025-06-02 09:37:06 -0300

    :boom: fix: Ajustando contagem e padding do card resultados

--- src/components/home/resultados/CardNumber.tsx
+++ src/components/home/resultados/CardNumber.tsx
@@ -3,34 +3,34 @@ import ConterNumber from "./ConterNumber";
 
 const CardNumber = () => {
   return (
-    <div className="relative bg-azul-escuro w-full lg:p-[7.2rem] p-[5rem] rounded-4xl">
+    <div className="relative bg-azul-escuro w-full lg:p-[7.2rem] p-[5rem] rounded-4xl lg:h-[372.781px]">
       <div className="flex flex-col md:flex-row gap-10 md:gap-20 justify-around text-white ">
         <div className="flex flex-col lg:flex-row gap-10 md:gap-20">
           <div className="flex flex-col items-center">
-            <h3 className="text-5xl sm:text-[5.6875rem] font-bold">
+            <h3 className="text-5xl sm:text-[4.9875rem] font-bold">
               <ConterNumber to={840} />
             </h3>
-            <p className="text-center text-sm sm:text-[1.125rem]">Média das redações</p>
+            <p className="text-center text-sm sm:text-[1.125rem] w-[200%]">Média das redações</p>
           </div>
           <div className="flex flex-col items-center">
-            <h3 className="text-5xl sm:text-[5.6875rem] font-bold">
-              <ConterNumber to={2} />K+
+            <h3 className="text-5xl sm:text-[4.9875rem] font-bold">
+              <ConterNumber to={2000} plus />
             </h3>
-            <p className="text-center text-sm sm:text-[1.125rem]">Estudantes impactados</p>
+            <p className="text-center text-sm sm:text-[1.125rem] w-[200%]">Estudantes impactados</p>
           </div>
         </div>
         <div className="flex flex-col lg:flex-row gap-10 md:gap-20">
           <div className="flex flex-col items-center">
-            <h3 className="text-5xl sm:text-[5.6875rem] font-bold">
+            <h3 className="text-5xl sm:text-[4.9875rem] font-bold">
               <ConterNumber to={480} />
             </h3>
-            <p className="text-center text-sm sm:text-[1.125rem]">Texto de exemplo</p>
+            <p className="text-center text-sm sm:text-[1.125rem] w-[200%]">Texto de exemplo</p>
           </div>
           <div className="flex flex-col items-center">
-            <h3 className="text-5xl sm:text-[5.6875rem] font-bold">
-              <ConterNumber to={47} />+
+            <h3 className="text-5xl sm:text-[4.9875rem] font-bold">
+              <ConterNumber to={47} plus />
             </h3>
-            <p className="text-center text-sm sm:text-[1.125rem]">Texto</p>
+            <p className="text-center text-sm sm:text-[1.125rem] w-[200%]">Texto</p>
           </div>
         </div>
       </div>
