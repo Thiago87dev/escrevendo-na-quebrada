dde950a4cc73fd91ab83fa50da27f371b77d9c33
thiago alves <thiago87gsa@gmail.com>
2025-06-11 11:25:03 -0300

    :boom: fix: rodape links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+   images: {
+    unoptimized: true, // <- Adicione isso aqui
+  },
 };
 
 export default nextConfig;
